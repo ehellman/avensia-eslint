142016001212098f0705fb285ba6fb14c5fd9d1e
Erik Hellman <iam@erikhellman.com>
2022-05-30 16:27:22 +0200

    Remove unused code

--- tests/index.js
+++ tests/index.js
@@ -25,9 +25,6 @@ describe('all rule files should have a corresponding test', () => {
 
   ruleFiles.forEach((ruleName) => {
     rules.push(ruleName);
-    // it(`should export ${ruleName}`, () => {
-    //   assert.equal(plugin.rules[ruleName], require(path.join('../lib/rules', ruleName))); // eslint-disable-line global-require, import/no-dynamic-require
-    // });
   });
 
   ruleTestFiles.forEach((ruleName) => {
